b2854768172fac58bc93d8ac8881e8dbac678c74
Gonior <43252085+Gonior@users.noreply.github.com>
2019-09-21 23:38:47 +0700

    Dibuat : Sabtu, 21 September 2019

---
+++ src/pbo11k/pkg10118901/latihan10/logika/PBO11K10118901Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan10.logika;
+/**
+ *
+ * @author 
+ * NAMA : Dedi Cahya 
+ * KELAS : IF-11-K 
+ * NIM : 10118901 
+ * DESKRIPSI : Mengenan lebih jauh tipe data boolean (logika)
+ */
+public class PBO11K10118901Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 17;
+        boolean dewasa;
+        dewasa = (umur>=17);
+        System.out.println("Status dewasa : " +dewasa);
+    }
+    
+}
